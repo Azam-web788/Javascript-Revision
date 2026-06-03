a6f0b61b16542f77af9e3fe9fc0d5355f7de6009
Azam-web788 <azamkhan505560@gmail.com>
2026-06-03 20:42:25 +0500

    string topic completed

---
+++ String/app.js
@@ -0,0 +1,36 @@
+let name = "Azam"
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+// console.log(name[4]);
+
+console.log(name.length);
+
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+
+let real_name = "Muhammad Azam"
+console.log(real_name.startsWith("Mu"));
+console.log(real_name.endsWith("Az"));
+
+// console.log(real_name.lastIndexOf(" A"));
+
+// console.log(real_name.charAt(100));
+
+// let b = "AzhamAz"
+// console.log(real_name.slice(2));
+// console.log(b.replace("Az" , "github"));
+// console.log(b.replaceAll("Az" , "github"));
+
+// console.log(real_name.concat(" Khan" , b , "Yes" , "Hello" , "World"));
+// let example = " Azam "
+// console.log(example);
+// console.log(example.trim());
+
+var jhoot = "Azam is a good cricketer. He is a fittest player after virat kohli in the world. Azam is looking like a wow. He passed all the fitness test. He followed diet strictly."
+console.log(jhoot.replaceAll("Azam" , "Abdullah"));
+
+
+
+
