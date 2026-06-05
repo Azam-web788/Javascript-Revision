8d8fb134315fb854ef3af943b79e25fece2be2d3
Azam-web788 <azamkhan505560@gmail.com>
2026-06-05 22:57:17 +0500

    array topic complete

---
+++ Array/app.js
@@ -0,0 +1,55 @@
+console.log("Hello world");
+
+let arr = [1 , 2, 3, 4, 5, 6]
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr.length);
+console.log(arr);
+let fruits = ["apple" , 20 , true , ["strawberry" , "pineapple" , "nashpati" , ['banana' , "orange" , ["anar" , "anar2"]] , "peech"] , "grapes"]
+console.log(fruits[3][3][2][0]);
+
+// let adding_item = []
+
+// adding_item[0] = prompt("Enter one item")
+// adding_item[1] = prompt("Enter second item")
+// adding_item[2] = prompt("Enter thired item")
+
+// console.log(adding_item);
+
+// Array methods
+
+// push
+let fruit = ["watermelon" , "strawberry" , "pineapple" , "nashpati"]
+// fruit.push("mango")
+
+// pop
+// fruit.pop()
+// unshift
+// fruit.unshift("apple")
+// shift
+// fruit.shift()
+// console.log(fruit);
+
+// splice
+// fruit.splice(1 , 2 , "apple")
+// slice
+// const new_arr = fruit.slice(0 , 3)
+// console.log(new_arr);
+console.log(fruit);
+// split
+let name = "Muhammad-Azam-khan"
+// console.log(name.split("-"));
+// join
+let name_arr = ["Muhammad" , "Azam" , "Khan"]
+// console.log(name_arr.join("="));
+
+// concat
+// console.log(name_arr.concat(fruit));
+
+console.log(arr.indexOf(2));
+
+
